890bd98f39457b66209a20dd052057ec17e61f70
huzaifadahmed <40344718+huzaifadahmed@users.noreply.github.com>
2018-07-11 13:18:45 -0400

    Create MEC733_I_O.h

---
+++ MEC733_I_O.h
@@ -0,0 +1,93 @@
+  # define DDRB0 0
+  # define DDRB1 1
+  # define DDRB2 2
+  # define DDRB3 3
+  # define DDRB4 4
+  # define DDRB5 5
+  # define DDRB6 6
+  # define DDRB7 7
+  
+  # define PORTB0 0
+  # define PORTB1 1
+  # define PORTB2 2
+  # define PORTB3 3
+  # define PORTB4 4
+  # define PORTB5 5
+  # define PORTB6 6
+  # define PORTB7 7
+  
+  # define PINB0 0
+  # define PINB1 1
+  # define PINB2 2
+  # define PINB3 3
+  # define PINB4 4
+  # define PINB5 5
+  # define PINB6 6
+  # define PINB7 7
+  
+  # define DDRC0 0
+  # define DDRC1 1
+  # define DDRC2 2
+  # define DDRC3 3
+  # define DDRC4 4
+  # define DDRC5 5
+  # define DDRC6 6
+  # define DDRC7 7
+  
+  # define PORTC0 0
+  # define PORTC1 1
+  # define PORTC2 2
+  # define PORTC3 3
+  # define PORTC4 4
+  # define PORTC5 5
+  # define PORTC6 6
+  # define PORTC7 7
+  
+  # define PINC0 0
+  # define PINC1 1
+  # define PINC2 2
+  # define PINC3 3
+  # define PINC4 4
+  # define PINC5 5
+  # define PINC6 6
+  # define PINC7 7
+  
+  # define DDRD0 0
+  # define DDRD1 1
+  # define DDRD2 2
+  # define DDRD3 3
+  # define DDRD4 4
+  # define DDRD5 5
+  # define DDRD6 6
+  # define DDRD7 7
+  
+  # define PORTD0 0
+  # define PORTD1 1
+  # define PORTD2 2
+  # define PORTD3 3
+  # define PORTD4 4
+  # define PORTD5 5
+  # define PORTD6 6
+  # define PORTD7 7
+  
+  # define PIND0 0
+  # define PIND1 1
+  # define PIND2 2
+  # define PIND3 3
+  # define PIND4 4
+  # define PIND5 5
+  # define PIND6 6
+  # define PIND7 7
+  
+  
+  #define PINB _SFR_IO8(0x03)
+  #define DDRB _SFR_IO8(0x04)
+  #define PORTB _SFR_IO8(0x05)
+  
+  #define PINC _SFR_IO8(0x06)
+  #define DDRC _SFR_IO8(0x07)
+  #define PORTC _SFR_IO8(0x08)
+  
+  #define PIND _SFR_IO8(0x09)
+  #define DDRD _SFR_IO8(0x0A)
+  #define PORTD _SFR_IO8(0x0B)
